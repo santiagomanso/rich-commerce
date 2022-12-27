100cd1517d78f471fae8b652136e2371dc9697ac
santiagomanso <santiagomanso@web.de>
2022-12-27 19:22:47 +0100

    ➖ removed old untracked files

--- src/views/CartScreen/CartScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import useProducts from '../../hooks/useCategories'
-import Product from '../../components/Category/Category'
-const CartScreen = () => {
-  const { loading, error, products, setProducts } = useProducts()
-  return <Product product={products[4]}>CartScreen</Product>
-}
-
-export default CartScreen
--- src/views/ProfileScreen/ProfileScreen.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ProfileScreen = () => {
-  return <div>ProfileScreen</div>
-}
-
-export default ProfileScreen
