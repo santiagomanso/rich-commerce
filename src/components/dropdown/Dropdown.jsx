0bd3815082519623d15e005f35f0a42656cbf542
santiagomanso <santiagomanso@web.de>
2022-12-27 19:15:49 +0100

    ⚡️ added props and logout

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -3,7 +3,7 @@ import { useRef } from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 
-const Dropdown = () => {
+const Dropdown = ({ name, logout }) => {
   const [active, setActive] = useState(false)
   const menuRef = useRef()
 
@@ -24,13 +24,13 @@ const Dropdown = () => {
     <div
       ref={menuRef}
       onClick={() => setActive(!active)}
-      className={`relative group hover:border-slate-600 cursor-pointer ${
+      className={`relative  hover:border-slate-600 cursor-pointer ${
         active === 'profile'
           ? 'border-b-4 border-slate-600'
           : 'border-b-4 border-transparent'
       } flex items-center gap-2`}
     >
-      <span>Profile</span>
+      <span>{name}</span>
       <i
         className={` fa-solid fa-chevron-down text-xl text-slate-600 duration-500
                 ${active ? 'rotate-180' : 'rotate-0'}`}
@@ -39,18 +39,21 @@ const Dropdown = () => {
       <ul
         className={`flex duration-150 ${
           active ? 'scale-100' : 'scale-0'
-        } flex-col gap-3 absolute top-10 right-0 shadow-lg z-10 w-56 bg-slate-100 p-4  rounded-md`}
+        } flex-col gap-3 absolute top-10 right-0 shadow-lg z-10 w-56 bg-slate-100 p-2 rounded-md`}
       >
-        <li className='flex items-center gap-2'>
-          <i className='fa-solid fa-circle-user text-slate-600'></i>
+        <li className='flex items-center gap-2 hover:bg-slate-300 p-4 rounded-md group'>
+          <i className='fa-solid fa-circle-user text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0 group-hover:scale-150 duration-500'></i>
           <span>My profile</span>
         </li>
-        <li className='flex items-center gap-2'>
-          <i className='fa-solid fa-heart text-slate-600'></i>
+        <li className='flex items-center gap-2 hover:bg-slate-300 p-4 rounded-md group'>
+          <i className='fa-solid fa-heart text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0 group-hover:scale-150 duration-500'></i>
           <span>Favourites</span>
         </li>
-        <li className='flex items-center gap-2'>
-          <i className='fa-solid fa-power-off text-slate-600'></i>
+        <li
+          className='flex items-center gap-2 hover:bg-slate-300 p-4 rounded-md group'
+          onClick={logout}
+        >
+          <i className='fa-solid fa-power-off text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0 group-hover:scale-150 duration-500'></i>
           <span>Logout</span>
         </li>
       </ul>
