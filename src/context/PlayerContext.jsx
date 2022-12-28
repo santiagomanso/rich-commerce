0f7b21d6a0703c2f8995b68b748e633aeb44ba71
santiagomanso <santiagomanso@web.de>
2022-12-28 21:20:02 +0100

    ⚡️ improved props obj instead of array

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -6,7 +6,7 @@ export const PlayerProvider = (props) => {
   const [player, setPlayer] = useState('') //single character that plays troughout the app
 
   return (
-    <PlayerContext.Provider value={[player, setPlayer]}>
+    <PlayerContext.Provider value={{ player, setPlayer }}>
       {props.children}
     </PlayerContext.Provider>
   )
