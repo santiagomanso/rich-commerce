8b39fea2050fe561298c26361d808c00c6ea5cae
santiagomanso <santiagomanso@web.de>
2022-12-28 21:19:37 +0100

    🚧 msg to display on redirect, future feature

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ const ProtectedRoute = ({ children }) => {
   const { user } = UserAuth()
 
   if (!user) {
-    return <Navigate to='/login' />
+    return <Navigate to='/login' msg='You must login before' />
   } else {
     return children
   }
