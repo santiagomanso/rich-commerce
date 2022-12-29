9c26701deba4c534246207e27772f078ece52077
santiagomanso <santiagomanso@web.de>
2022-12-29 22:16:47 +0100

    🔨 created HowToPlay component!

---
+++ src/Pages/HowToPlayScreen/HowToPlayScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HowToPlayScreen = () => {
+  return (
+    <div className='lg:mt-10'>
+      <span>Getting started</span>
+      <h1>How To Play Billionaire eCommerce!</h1>
+      <span className='text-white font-medium bg-slate-600 px-2 py-1 rounded'>
+        1
+      </span>
+    </div>
+  )
+}
+
+export default HowToPlayScreen
