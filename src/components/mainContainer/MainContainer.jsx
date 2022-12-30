e30ff604bdd9dff80f03996cc2d1b4af272ba1af
santiagomanso <santiagomanso@web.de>
2022-12-30 22:56:40 +0100

    🩹 fixed 3-way gradient color, from css to inline

--- src/components/mainContainer/MainContainer.jsx
+++ src/components/mainContainer/MainContainer.jsx
@@ -1,7 +1,12 @@
 import './mainContainer.css'
 const MainContainer = ({ children }) => {
   return (
-    <div className='outterDiv'>
+    <div
+      className='outterDiv bg-gradient-to-br
+     from-gray-200 dark:from-slate-800
+     via-gray-200  dark:via-stone-700
+     to-slate-400  dark:to-slate-800'
+    >
       <div className='innerDiv'>{children}</div>
     </div>
   )
