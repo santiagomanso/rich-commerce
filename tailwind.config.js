f97bc885b491f471dddba36626db475a2f89546f
santiagomanso <santiagomanso@web.de>
2023-01-06 00:28:39 +0100

    🔥 added 3 blob animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,64 @@ module.exports = {
       fontFamily: {
         primary: 'Poppins',
       },
+      animation: {
+        blobLeft: 'blobLeft 4s infinite',
+        blobRight: 'blobRight 7s infinite',
+        blobBottom: 'blobBottom 10s infinite',
+      },
+      keyframes: {
+        blobLeft: {
+          '0%': {
+            transform: 'translate(0%, 0%) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(10%, -20%) scale(1.2)',
+          },
+          '66%': {
+            transform: 'translate(-20%, 20%) scale(0.8)',
+          },
+          '90%': {
+            transform: 'translate(50%, 3%) scale(0.4)',
+          },
+          '100%': {
+            transform: 'translate(0%, 0%) scale(1)',
+          },
+        },
+        blobRight: {
+          '0%': {
+            transform: 'translate(0%, 0%) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(-90%, 70%) scale(1.2)',
+          },
+          '66%': {
+            transform: 'translate(-100%, -55%) scale(0.8)',
+          },
+          '90%': {
+            transform: 'translate(-50%, -3%) scale(0.4)',
+          },
+          '100%': {
+            transform: 'translate(0%, 0%) scale(1)',
+          },
+        },
+        blobBottom: {
+          '0%': {
+            transform: 'translate(0%, 0%) scale(0.5)',
+          },
+          '33%': {
+            transform: 'translate(25%, -70%) scale(1.2)',
+          },
+          '66%': {
+            transform: 'translate(-25%, -80%) scale(0.8)',
+          },
+          '90%': {
+            transform: 'translate(25%, -70%) scale(0.4)',
+          },
+          '100%': {
+            transform: 'translate(0%, 0%) scale(1)',
+          },
+        },
+      },
     },
   },
   plugins: [],
