8bd3e27553d737dbd44e8c24d3afbf7d38b7d590
santiagomanso <santiagomanso@web.de>
2023-01-06 00:30:44 +0100

    🔥 added blob spheres maybe later to main app.js

--- src/components/mainContainer/MainContainer.jsx
+++ src/components/mainContainer/MainContainer.jsx
@@ -1,4 +1,5 @@
 import './mainContainer.css'
+import FloatingSpheres from '../FloatingSpheres/FloatingSpheres'
 const MainContainer = ({ children }) => {
   return (
     <div
@@ -7,6 +8,7 @@ const MainContainer = ({ children }) => {
      via-gray-200  dark:via-slate-900
      to-slate-400  dark:to-black/95'
     >
+      <FloatingSpheres />
       <div className='innerDiv'>{children}</div>
     </div>
   )
