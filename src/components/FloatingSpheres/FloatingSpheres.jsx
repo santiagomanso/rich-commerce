2544f2cfb51ca0c49df5a58a8d0cff96b7becee0
santiagomanso <santiagomanso@web.de>
2023-01-07 10:20:29 +0100

    🔧 reworked css

--- src/components/FloatingSpheres/FloatingSpheres.jsx
+++ src/components/FloatingSpheres/FloatingSpheres.jsx
@@ -1,10 +1,10 @@
 const FloatingSpheres = () => {
   return (
-    <>
-      <div className='absolute top-[20%] left-0 w-[400px] h-[400px] bg-pink-600 rounded-full mix-blend-multiply filter  animate-blobLeft blur-3xl opacity-30' />
-      <div className='absolute top-[2%] right-10 w-[400px] h-[400px] bg-sky-600 rounded-full mix-blend-multiply filter  animate-blobRight blur-3xl opacity-30' />
-      <div className='absolute bottom-[5%] right-[50%] translate-x-[50%] w-[400px] h-[400px] bg-amber-500 rounded-full mix-blend-multiply filter  animate-blobBottom blur-3xl opacity-30' />
-    </>
+    <div className='hidden lg:inline-block'>
+      <div className='absolute top-[20%] left-0 w-[450px] h-[450px] bg-pink-600 dark:bg-indigo-500 rounded-full mix-blend-multiply dark:mix-blend-normal filter pointer-events-none blur-xl opacity-40 dark:opacity-20  animate-blobLeft ' />
+      <div className='absolute top-[2%] right-10 w-[400px] h-[400px] bg-sky-600 dark:bg-fuchsia-400 rounded-full mix-blend-multiply dark:mix-blend-normal filter pointer-events-none blur-xl opacity-40 dark:opacity-20  animate-blobRight ' />
+      <div className='absolute bottom-[5%] right-[50%] translate-x-[50%] w-[450px] h-[450px] bg-amber-500 rounded-full mix-blend-multiply dark:mix-blend-normal filter pointer-events-none blur-xl opacity-40 dark:opacity-20  animate-blobBottom ' />
+    </div>
   )
 }
 
