211a0bbdb07b1e37c4fcafb8e14e0b0bbdc5d929
santiagomanso <santiagomanso@web.de>
2023-01-10 16:20:22 +0100

    ✨ feature attempted player, to set after redirect

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -4,9 +4,12 @@ export const PlayerContext = createContext()
 
 export const PlayerProvider = (props) => {
   const [player, setPlayer] = useState('') //single character that plays troughout the app
+  const [attemptedPlayer, setAttemptedPlayer] = useState('')
 
   return (
-    <PlayerContext.Provider value={{ player, setPlayer }}>
+    <PlayerContext.Provider
+      value={{ player, setPlayer, attemptedPlayer, setAttemptedPlayer }}
+    >
       {props.children}
     </PlayerContext.Provider>
   )
