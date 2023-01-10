26727bcfb3d4e305c1a46f362ace52f891dbf521
santiagomanso <santiagomanso@web.de>
2023-01-10 15:43:41 +0100

    ➕ added cart context

--- src/App.js
+++ src/App.js
@@ -10,54 +10,75 @@ import CharactersScreen from './Pages/CharacterScreen/CharactersScreen'
 import FloatingBudget from './components/FloatingBudget/FloatingBudget'
 import { PlayerProvider } from './context/PlayerContext'
 import { AuthContextProvider } from './context/AuthContext'
+import { CartProvider } from './context/CartContext'
+
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 import HowToPlayScreen from './Pages/HowToPlayScreen/HowToPlayScreen'
 import ProductsScreen from './Pages/ProductsScreen/ProductsScreen'
+import { RedirectProvider } from './context/RedirectContext'
+import CategoriesScreen from './Pages/CategoriesScreen/CategoriesScreen'
 
 function App() {
   return (
     <AuthContextProvider>
       <PlayerProvider>
-        <BrowserRouter>
-          <MainContainer>
-            <Navbar />
-            <FloatingBudget />
-            <Routes>
-              <Route exact path='/' element={<HomeScreen />} />
-              <Route
-                exact
-                path='/characters'
-                element={
-                  <ProtectedRoute>
-                    <CharactersScreen />
-                  </ProtectedRoute>
-                }
-              />
-              <Route exact path='/profile' element={<ProfileScreen />} />
-              <Route exact path='/login' element={<LoginScreen />} />
-              <Route exact path='/howtoplay' element={<HowToPlayScreen />} />
-              <Route
-                exact
-                path='/cart'
-                element={
-                  <ProtectedRoute>
-                    <CartScreen />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                exact
-                path='/category/:categoryId'
-                element={
-                  <ProtectedRoute>
-                    <ProductsScreen />
-                  </ProtectedRoute>
-                }
-              />
-            </Routes>
-            <Footer />
-          </MainContainer>
-        </BrowserRouter>
+        <CartProvider>
+          <RedirectProvider>
+            <BrowserRouter>
+              <MainContainer>
+                <Navbar />
+                <FloatingBudget />
+                <Routes>
+                  <Route exact path='/' element={<HomeScreen />} />
+                  <Route
+                    exact
+                    path='/characters'
+                    element={
+                      <ProtectedRoute>
+                        <CharactersScreen />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route exact path='/profile' element={<ProfileScreen />} />
+                  <Route exact path='/login' element={<LoginScreen />} />
+                  <Route
+                    exact
+                    path='/howtoplay'
+                    element={<HowToPlayScreen />}
+                  />
+                  <Route
+                    exact
+                    path='/cart'
+                    element={
+                      <ProtectedRoute>
+                        <CartScreen />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    exact
+                    path='/categories'
+                    element={
+                      <ProtectedRoute>
+                        <CategoriesScreen />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route
+                    exact
+                    path='/category/:categoryId'
+                    element={
+                      <ProtectedRoute>
+                        <ProductsScreen />
+                      </ProtectedRoute>
+                    }
+                  />
+                </Routes>
+                <Footer />
+              </MainContainer>
+            </BrowserRouter>
+          </RedirectProvider>
+        </CartProvider>
       </PlayerProvider>
     </AuthContextProvider>
   )
