06494eb974e626c04c79a416c219bc65bec1171c
santiagomanso <santiagomanso@web.de>
2023-01-10 16:21:40 +0100

    🔨 added cart screen

--- src/Pages/CartScreen/CartScreen.jsx
+++ src/Pages/CartScreen/CartScreen.jsx
@@ -1,5 +1,29 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { CartContext } from '../../context/CartContext'
+
 const CartScreen = () => {
-  return <div>CartScreen</div>
+  const { cart } = useContext(CartContext)
+  return (
+    <main className='h-full lg:h-5/6 flex justify-center items-center'>
+      {cart.length > 0 ? (
+        ''
+      ) : (
+        <div className='animate__animated animate__tada flex flex-col gap-y-5 items-center lg:text-6xl  rounded-lg'>
+          <div className='flex items-center gap-2'>
+            <span>Your Cart is Empty</span>
+            <i className='fa-regular fa-face-frown text-gray-700 dark:text-gray-300'></i>
+          </div>
+          <Link
+            to='/categories'
+            className='bg-gradient-to-br from-indigo-500 to-purple-500 text-white px-6 py-3 rounded-md'
+          >
+            Shop now!
+          </Link>
+        </div>
+      )}
+    </main>
+  )
 }
 
 export default CartScreen
