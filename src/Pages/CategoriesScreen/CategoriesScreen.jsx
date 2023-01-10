2587f6598bb415bd1c112bd8059d66144228071b
santiagomanso <santiagomanso@web.de>
2023-01-10 16:21:55 +0100

    🔨 added categories screen

---
+++ src/Pages/CategoriesScreen/CategoriesScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CategoriesGrid from '../../components/grids/CategoriesGrid'
+import useCategories from '../../hooks/useCategories'
+
+const CategoriesScreen = () => {
+  const { loading, error, categories, setCategories } = useCategories()
+  return (
+    <section className='animate__animated animate__fadeIn'>
+      <CategoriesGrid categories={categories} />
+    </section>
+  )
+}
+
+export default CategoriesScreen
