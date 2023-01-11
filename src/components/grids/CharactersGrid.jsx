1a10c3f1431b0c044b754b9f0e56e977efa28333
santiagomanso <santiagomanso@web.de>
2023-01-11 17:14:17 +0100

    💫 added animation

--- src/components/grids/CharactersGrid.jsx
+++ src/components/grids/CharactersGrid.jsx
@@ -2,7 +2,7 @@ import Character from '../Character/Character'
 
 const CharactersGrid = ({ characters }) => {
   return (
-    <div className='mt-5 w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-10'>
+    <div className='animate__animated animate__fadeIn mt-5 w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-10'>
       {characters.length > 0
         ? characters.map((character) => (
             <Character character={character} key={character.rank} />
