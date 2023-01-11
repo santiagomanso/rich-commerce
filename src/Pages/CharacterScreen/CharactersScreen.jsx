98fbf820237ad70e5bd3cbf2bb9b385b6ccaa24e
santiagomanso <santiagomanso@web.de>
2023-01-11 17:14:50 +0100

    💫 added animation

--- src/Pages/CharacterScreen/CharactersScreen.jsx
+++ src/Pages/CharacterScreen/CharactersScreen.jsx
@@ -5,7 +5,7 @@ import SkeletonGrid from '../../components/grids/SkeletonGrid'
 const CharactersScreen = () => {
   const { characters, loading, error, setCharacters } = useCharacters()
   return (
-    <main>
+    <main className='animate__animated animate__fadeIn'>
       <h1 className='mt-14 font-semibold text-gray-600'>
         Select your billionaire
       </h1>
