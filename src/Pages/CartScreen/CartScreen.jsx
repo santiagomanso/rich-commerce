9f807a897751943cf20efb9be390dab52ceda027
santiagomanso <santiagomanso@web.de>
2023-01-13 17:21:21 +0100

    ✨ new feature checkout

--- src/Pages/CartScreen/CartScreen.jsx
+++ src/Pages/CartScreen/CartScreen.jsx
@@ -1,43 +1,122 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import AverageAnnualIncome from '../../components/CheckoutStats/AverageAnnualIncome'
+import NetworthImpacts from '../../components/CheckoutStats/NetworthImpacts'
+import OrderSummary from '../../components/CheckoutStats/OrderSummary'
+import CountryDropdown from '../../components/CountryDropdown/CountryDropdown'
 import { CartContext } from '../../context/CartContext'
 import { PlayerContext } from '../../context/PlayerContext'
 
 const CartScreen = () => {
   const { cart, cartCount } = useContext(CartContext)
   const { player } = useContext(PlayerContext)
+
+  const [summaryTotal, setSummaryTotal] = useState(0)
+  const [discount, setDiscount] = useState(0)
+
+  //annual income section
+  const [yearsToAffordCart, setYearsToAffordCart] = useState(0)
+  const [annualIncome, setAnnualIncome] = useState(38426) //germany to initialize
+
+  useEffect(() => {
+    getSemitotals()
+    getDiscounts()
+  }, [cartCount])
+
+  const getSemitotals = () => {
+    let total = 0
+    cart.map(({ item }) => {
+      return (total = total + item.price)
+    })
+    return total
+  }
+
+  const getDiscounts = () => {
+    console.log('cartCount', cartCount)
+    switch (true) {
+      case cartCount === 1: {
+        setDiscount(0.1)
+        break
+      }
+      case cartCount > 1 && cartCount < 3: {
+        setDiscount(0.2)
+        break
+      }
+      case cartCount >= 3 && cartCount < 6: {
+        setDiscount(0.3)
+        break
+      }
+      case cartCount > 6: {
+        setDiscount(0.4)
+        break
+      }
+
+      default:
+        setDiscount(0)
+    }
+  }
+
+  const getTotals = () => {
+    return getSemitotals() - getSemitotals() * discount
+  }
+
+  const getPlayerNetworth = () => {
+    const initialMoney = player.estWorthPrev
+    console.log('initialMoney', initialMoney)
+    const moneyParts = initialMoney.toString()
+    console.log('moneyParts', moneyParts)
+    const networth = moneyParts.replace('.', '')
+    console.log('networth', +networth)
+    return +networth
+  }
+
+  const getBudgetTotal = (fn) => {
+    return Math.floor((getTotals() * 100) / fn)
+  }
+
+  const getYearsToCart = (income) => {
+    return Math.floor(getTotals() / income)
+  }
+
   return (
     <main
-      className={` animate__animated animate__fadeIn ${
+      className={`animate__animated animate__fadeIn ${
         cart.length > 0
           ? 'h-full lg:h-5/6'
           : 'h-full lg:h-5/6 flex justify-center items-center'
       } `}
     >
       {cart.length > 0 ? (
-        <div className='bg-blue-500 flex h-full'>
-          <div className='bg-red-300 w-2/3 h-full overflow-auto'>
-            <div className='bg-orange-300 col-span-2 flex flex-col gap-5'>
-              {cart.map(({ item }) => (
-                <div key={item.product_id} className='flex  bg-green-200'>
-                  <div className='max-w-sm'>
+        <div className='flex h-full'>
+          <div className='bg-gray-300/20 dark:bg-black/70 w-2/3 h-full overflow-auto px-2 rounded'>
+            <div className='col-span-2 flex flex-col gap-5'>
+              {cart.map(({ item }, i) => (
+                <div
+                  key={i}
+                  className={`flex  ${
+                    i % 2 === 0
+                      ? 'bg-gradient-to-br from-slate-100 via-slate-200 to-slate-400 dark:from-gray-700/50'
+                      : 'bg-gradient-to-br from-slate-100 via-slate-200 to-slate-400 dark:from-gray-700/50'
+                  } rounded`}
+                >
+                  <div className='max-w-[250px] p-2'>
                     <img
                       src={item.img}
                       alt={item.name}
-                      className='h-full w-full rounded-lg'
+                      className='h-full w-full rounded'
                     />
                   </div>
-                  <div className='flex flex-col'>
-                    <h2 className='font-semibold uppercase'>{item.name}</h2>
+                  <div className='flex flex-col p-3'>
+                    <h3 className='font-semibold uppercase'>{item.name}</h3>
                     {/* <p className=' max-w-[300px] text-ellipsis'>{item.desc}</p> */}
                   </div>
                 </div>
               ))}
             </div>
           </div>
-          <div className='bg-green-300 w-1/3 overflow-y-auto'>
-            <div className='bg-gray-300 p-4 rounded'>
-              <div className='bg-gray-800 py-3 px-4 rounded'>
+          <div className='w-1/3 overflow-y-auto'>
+            <div className='bg-gray-300 dark:bg-transparent p-4 rounded'>
+              <div className='bg-gray-800 dark:bg-gray-700/50 py-3 px-4 rounded'>
                 <h2 className='text-gray-200'>CHECKOUT</h2>
               </div>
               <span className='text-end'>
@@ -46,79 +125,33 @@ const CartScreen = () => {
                 est, ea adipisci ex eos natus voluptas sit reprehenderit
                 deleniti molestias. Blanditiis.
               </span>
-              <h2 className='mt-2 font-semibold text-gray-800'>
-                ORDER SUMMARY
-              </h2>
-              <div className='flex flex-col gap-y-3 divide-y-2 divide-gray-400 bg-white p-4 rounded'>
-                <span className='flex justify-between'>
-                  <span>
-                    Product{cartCount > 1 ? 's' : ''} Quantit
-                    {cartCount > 1 ? 'ies' : 'y'}
-                  </span>
-                  <span>{cartCount}</span>
-                </span>
-                <span className='flex justify-between'>
-                  <span>
-                    Product{cartCount > 1 ? 's' : ''} Total
-                    {cartCount > 1 ? 's' : ''}
-                  </span>
-                  <span>1223040349 $</span>
-                </span>
-                <span className='flex justify-between'>
-                  <span>Delivery</span>
-                  <span>FREE</span>
-                </span>
-                <span className='font-semibold text-2xl flex justify-between'>
-                  <span>Total</span>
-                  <span>31281378722 $</span>
-                </span>
-              </div>
-              <h2 className='mt-8 font-semibold text-gray-800'>
-                BUDGET IMPACTS
-              </h2>
-              <div className='flex flex-col gap-y-3 divide-y-2 divide-gray-400 bg-white p-4 rounded'>
-                <span className='flex justify-between'>
-                  <span>Player</span>
-                  <span className='font-semibold'>{player.personName}</span>
-                </span>
-                <span className='flex justify-between'>
-                  <span>Budget</span>
-                  <span>${player.estWorthPrev}</span>
-                </span>
-                <span className='flex justify-between'>
-                  <span>Cart cost</span>
-                  <span>31281378722 $</span>
-                </span>
-                <span className='flex justify-between text-2xl font-semibold'>
-                  <span>Total % of budget</span>
-                  <span>80%</span>
-                </span>
-              </div>
-              <h2 className='mt-8 font-semibold text-gray-800'>
-                AVERAGE PERSON
-              </h2>
-              <div className='flex flex-col gap-y-3 divide-y-2 divide-gray-400 bg-white p-4 rounded'>
-                <span className='flex justify-between'>
-                  <span>Annual Income</span>
-                  <span>${player.estWorthPrev}</span>
-                </span>
-                <span className='flex justify-between'>
-                  <span>Cart cost</span>
-                  <span>31281378722 $</span>
-                </span>
-                <span className='flex justify-between'>
-                  <span>Total % of budget</span>
-                  <span>200000%</span>
-                </span>
-                <span className='flex justify-between text-xl font-semibold'>
-                  <span>Years to afford cart</span>
-                  <span>40000 years</span>
-                </span>
-              </div>
+              <OrderSummary
+                cartCount={cartCount}
+                getSemitotals={getSemitotals}
+                discount={discount}
+                getTotals={getTotals}
+              />
+              <NetworthImpacts
+                name={player.personName}
+                getBudgetTotal={getBudgetTotal}
+                getPlayerNetworth={getPlayerNetworth}
+                getTotals={getTotals}
+              />
+              <AverageAnnualIncome
+                annualIncome={annualIncome}
+                getBudgetTotal={getBudgetTotal}
+                getTotals={getTotals}
+                getYearsToCart={getYearsToCart}
+                setAnnualIncome={setAnnualIncome}
+              />
               <h2 className='mt-8 font-semibold text-gray-800'>
                 GDP to {player.personName} Budget in %
               </h2>
-              <div className='flex flex-col gap-y-3 divide-y-2 divide-gray-400 bg-white p-4 rounded'>
+              <div className='flex flex-col gap-y-3 divide-y-2 divide-gray-400 bg-white dark:bg-gray-700/50 p-4 rounded'>
+                <span className='flex justify-between'>
+                  <span>Annual income per capita</span>
+                  <span>{annualIncome}</span>
+                </span>
                 <span className='flex justify-between'>
                   <span>Germany</span>
                   <span>0.13</span>
@@ -149,9 +182,10 @@ const CartScreen = () => {
                 </span>
               </div>
               <h2 className='mt-8 font-semibold text-gray-800'>
-                How many people makes 1 {player.personName}?
+                How many people are needed to match 1 {player.personName}{' '}
+                networth?
               </h2>
-              <div className='flex flex-col gap-y-3 divide-y-2 divide-gray-400 bg-white p-4 rounded'>
+              <div className='flex flex-col gap-y-3 divide-y-2 divide-gray-400 bg-white dark:bg-gray-700/50 p-4 rounded'>
                 <span className='flex justify-between'>
                   <span>Germany</span>
                   <span>670.000 persons</span>
