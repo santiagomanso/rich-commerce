df5d3890b855547b08b44cfa4cc112318713ed9c
santiagomanso <santiagomanso@web.de>
2023-01-13 17:21:41 +0100

    🐛 added Link from RRD

--- src/Pages/ProductsScreen/ProductsScreen.jsx
+++ src/Pages/ProductsScreen/ProductsScreen.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import SkeletonGrid from '../../components/grids/SkeletonGrid'
 import Product from '../../components/Product/Product'
 import { categories_list } from '../../data/categories'
@@ -27,8 +27,18 @@ const ProductsScreen = () => {
   //map de prouct con filter
 
   return (
-    <div>
-      <h1 className='mt-14 mb-2 font-semibold text-gray-600'>
+    <main>
+      <Link
+        to='/categories'
+        className='flex gap-1 items-center mt-5 mb-5 hover:translate-x-1 duration-150'
+      >
+        <i className='fa-solid fa-chevron-left text-gray-600 text-xl'></i>
+        <span className='text-gray-600 text-xl font-medium'>
+          Back to categories
+        </span>
+      </Link>
+
+      <h1 className='mt-4 mb-2 font-semibold text-gray-600'>
         {categoryName.name}
       </h1>
       {/* from a category, a collection of products get rendered */}
@@ -41,7 +51,7 @@ const ProductsScreen = () => {
           ))}
         </div>
       )}
-    </div>
+    </main>
   )
 }
 
