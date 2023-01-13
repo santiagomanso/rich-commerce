01538eaf73f229ff08cb90bf79dd496dba45ca07
santiagomanso <santiagomanso@web.de>
2023-01-13 17:20:12 +0100

    📦 added country data

---
+++ src/data/countriesData.js
@@ -0,0 +1,44 @@
+export const countryList = [
+  {
+    id: 1,
+    name: 'Germany',
+    flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-germany2x.png',
+    annualIncome: 38426,
+    gdp: 3693204332230,
+  },
+  {
+    id: 2,
+    name: 'United States',
+    flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-usa2x.png',
+    annualIncome: 53329,
+    gdp: 19485394000000,
+  },
+  {
+    id: 3,
+    name: 'Argentina',
+    flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-argentina2x.png',
+    annualIncome: 7212,
+    gdp: 637430331479,
+  },
+  {
+    id: 4,
+    name: 'Spain',
+    flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-spain2x.png',
+    annualIncome: 22545,
+    gdp: 1314314164402,
+  },
+  {
+    id: 5,
+    name: 'Venezuela', //source: https://www.statista.com/statistics/370937/gross-domestic-product-gdp-in-venezuela/
+    flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-venezuela2x.png',
+    annualIncome: 3380,
+    gdp: 49520000000,
+  },
+  {
+    id: 6,
+    name: 'Switzerland',
+    flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-switzerland2x.png',
+    annualIncome: 63782,
+    gdp: 678965423322,
+  },
+]
