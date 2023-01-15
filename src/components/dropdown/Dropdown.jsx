8667eb2bfcf0c2107ef127328798dc7bbbb26d9c
santiagomanso <santiagomanso@web.de>
2023-01-15 17:41:49 +0100

    🎨 reworked css

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -49,16 +49,16 @@ const Dropdown = ({ name, logout, ulOptions }) => {
         }`}
       ></i>
 
-      <ul className={` ${ulOptions} ${active ? 'scale-100' : 'scale-0'}`}>
-        <li className='group'>
+      <ul className={` ${ulOptions} w-48 ${active ? 'scale-100' : 'scale-0'}`}>
+        <li className='group p-2'>
           <i className='fa-solid fa-circle-user text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0 group-hover:scale-150 duration-500'></i>
           <span>My profile</span>
         </li>
-        <li className='group'>
+        <li className='group p-2'>
           <i className='fa-solid fa-heart text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0 group-hover:scale-150 duration-500'></i>
           <span>Favourites</span>
         </li>
-        <li className='group' onClick={handleLogout}>
+        <li className='group p-2' onClick={handleLogout}>
           <i className='fa-solid fa-power-off text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0 group-hover:scale-150 duration-500'></i>
           <span>Logout</span>
         </li>
