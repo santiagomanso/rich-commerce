54a843a33ae432d29ec1ec3b26c302716ed80296
santiagomanso <santiagomanso@web.de>
2023-01-16 16:29:13 +0100

    ✨ new feature set/get cart from LocalStorage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,11 +3,24 @@ import { createContext, useState, useEffect } from 'react'
 export const CartContext = createContext()
 
 export const CartProvider = (props) => {
-  const [cart, setCart] = useState([]) //array of products
+  const getCartFromStorage = () => {
+    if (window.localStorage.getItem('cart')) {
+      return (
+        'JSON.parse(window.localStorage.getItem("cart"))',
+        JSON.parse(window.localStorage.getItem('cart'))
+      )
+    }
+    return []
+  }
+
+  const [cart, setCart] = useState(getCartFromStorage()) //array of products
   const [cartCount, setCartCount] = useState(cart.length)
 
   useEffect(() => {
     setCartCount(cart.length)
+    getCartFromStorage()
+    // window.localStorage.removeItem()
+    window.localStorage.setItem('cart', JSON.stringify(cart))
   }, [cart])
 
   const addToCart = (item) => {
