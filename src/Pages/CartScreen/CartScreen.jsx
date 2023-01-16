a3c46634b9f28e6d575b8ea2521126777bff2982
santiagomanso <santiagomanso@web.de>
2023-01-16 16:29:46 +0100

    🎨 reworked css margin tablet/pc

--- src/Pages/CartScreen/CartScreen.jsx
+++ src/Pages/CartScreen/CartScreen.jsx
@@ -258,7 +258,7 @@ const CartScreen = () => {
           </Link>
         </div>
       ) : (
-        <div className='animate__animated animate__tada flex flex-col gap-y-5 items-center lg:text-6xl  rounded-lg'>
+        <div className='pt-[50%] md:pt-[17%] animate__animated animate__tada flex flex-col gap-y-5 items-center lg:text-6xl  rounded-lg'>
           <div className='flex items-center gap-2'>
             <span>Your Cart is Empty</span>
             <i className='fa-regular fa-face-frown text-gray-700 dark:text-gray-300'></i>
