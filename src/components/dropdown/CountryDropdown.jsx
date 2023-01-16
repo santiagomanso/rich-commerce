7edbaf971ff97778366306e56134b12b79c2db76
santiagomanso <santiagomanso@web.de>
2023-01-16 13:36:57 +0100

    🔨 untracked file

---
+++ src/components/dropdown/CountryDropdown.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState, useRef } from 'react'
+import { countryList } from '../../data/countriesData'
+
+const CountryDropdown = ({ setAnnualIncome }) => {
+  const [active, setActive] = useState(false)
+  const menuRef = useRef()
+  const [country, setCountry] = useState('Germany')
+  const [flag, setFlag] = useState(
+    'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-germany2x.png'
+  )
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setActive(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handler)
+    return () => {
+      document.removeEventListener('mousedown', handler)
+    }
+  })
+
+  const handleChange = ({ name, flag, annualIncome }) => {
+    setCountry(name)
+    setFlag(flag)
+    setAnnualIncome(annualIncome)
+  }
+
+  return (
+    <div
+      ref={menuRef}
+      onClick={() => setActive(!active)}
+      className={`dropdownWrapper`}
+    >
+      <span className='flex gap-2 items-center'>
+        <span>{country}</span>
+        <img src={flag} alt={country} className='h-7 w-7' />
+      </span>
+      <i
+        className={`fa-solid fa-chevron-down ${
+          active ? 'rotate-[3deg]' : '-rotate-90'
+        }`}
+      ></i>
+
+      <ul
+        className={`${
+          active ? 'scale-100' : 'scale-0'
+        } h-[300px] overflow-auto w-56 p-3`}
+      >
+        {countryList.map((country) => {
+          return (
+            <li
+              className='flex items-center justify-between p-2'
+              key={country.id}
+              onClick={() => handleChange(country)}
+            >
+              <span>{country.name}</span>
+              <img src={country.flag} alt={country.name} />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default CountryDropdown
