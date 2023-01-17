3723e0625abf73d10686edf8b9e99fe697a9a7b1
santiagomanso <santiagomanso@web.de>
2023-01-17 15:35:49 +0100

    ✨ new object translations

---
+++ src/utils/translations.js
@@ -0,0 +1,35 @@
+export const translations = {
+  es: {
+    //navbar
+    home: 'Inicio',
+    howToPlay: 'Como jugar',
+    shopNow: 'Comprar ya!',
+    players: 'Jugadores',
+    logIn: 'Iniciar sesion',
+    cart: 'Carrito',
+    //categories
+    categoriesH1: 'Nuestras categorias de lujosos productos',
+  },
+  en: {
+    //navbar
+    home: 'Home',
+    howToPlay: 'How to play',
+    shopNow: 'Show now!',
+    players: 'Players',
+    logIn: 'Log in',
+    cart: 'Cart',
+    //categories
+    categoriesH1: 'Our Categories of luxury assets',
+  },
+  de: {
+    //navbar
+    home: 'Startseite',
+    howToPlay: 'Wie zu verwenden',
+    shopNow: 'Jetzt einkaufen',
+    players: 'Spieler',
+    logIn: 'Einloggen',
+    cart: 'Einkaufswagen',
+    //categories
+    categoriesH1: 'Unsere Kategorien von Luxusgütern',
+  },
+}
