c9c0878ab5d2852137fd096df609fb4c8dee9b1f
santiagomanso <santiagomanso@web.de>
2023-01-18 16:23:57 +0100

    🔥 Language change ready

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState, useRef, useContext } from 'react'
 import { CartContext } from '../../context/CartContext'
+import { LanguageContext } from '../../context/LanguageContext'
 import { PlayerContext } from '../../context/PlayerContext'
 import { RedirectContext } from '../../context/RedirectContext'
 import './dropdown.css'
 
 const Dropdown = ({ name, logout, ulOptions }) => {
+  const { text, language } = useContext(LanguageContext)
   const { setPath } = useContext(RedirectContext)
   const { setPlayer } = useContext(PlayerContext)
   const { setCart } = useContext(CartContext)
@@ -46,15 +48,15 @@ const Dropdown = ({ name, logout, ulOptions }) => {
       <ul className={` ${ulOptions} w-48 ${active ? 'scale-100' : 'scale-0'}`}>
         <li className='group p-2'>
           <i className='fa-solid fa-circle-user text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0 group-hover:scale-150 duration-500'></i>
-          <span>My profile</span>
+          <span>{text.myProfile}</span>
         </li>
-        <li className='group p-2'>
+        {/* <li className='group p-2'>
           <i className='fa-solid fa-heart text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0 group-hover:scale-150 duration-500'></i>
           <span>Favourites</span>
-        </li>
+        </li> */}
         <li className='group p-2' onClick={handleLogout}>
           <i className='fa-solid fa-power-off text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0 group-hover:scale-150 duration-500'></i>
-          <span>Logout</span>
+          <span>{text.logOut}</span>
         </li>
       </ul>
     </div>
