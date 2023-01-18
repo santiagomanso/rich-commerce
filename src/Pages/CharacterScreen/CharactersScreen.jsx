4dc0f3dd0419245beef105bbd8e1862aa9b1d36b
santiagomanso <santiagomanso@web.de>
2023-01-18 16:27:16 +0100

    ⚡️ Language change ready

--- src/Pages/CharacterScreen/CharactersScreen.jsx
+++ src/Pages/CharacterScreen/CharactersScreen.jsx
@@ -1,13 +1,16 @@
 import CharactersGrid from '../../components/grids/CharactersGrid'
 import useCharacters from '../../hooks/useCharacters'
 import SkeletonGrid from '../../components/grids/SkeletonGrid'
+import { useContext } from 'react'
+import { LanguageContext } from '../../context/LanguageContext'
 
 const CharactersScreen = () => {
   const { characters, loading, error, setCharacters } = useCharacters()
+  const { text } = useContext(LanguageContext)
   return (
     <main className='animate__animated animate__fadeIn'>
       <h1 className='mt-14 font-semibold text-gray-600'>
-        Select your billionaire
+        {text.characterScreenH1}
       </h1>
       {loading ? <SkeletonGrid /> : <CharactersGrid characters={characters} />}
     </main>
