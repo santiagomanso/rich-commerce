77f42ec98b0153062a8f2ee3a196f60fcdfa4968
santiagomanso <santiagomanso@web.de>
2023-01-22 00:43:28 +0100

    🎨 🔧 /reworked logic -css

--- src/Pages/HowToPlayScreen/HowToPlayScreen.jsx
+++ src/Pages/HowToPlayScreen/HowToPlayScreen.jsx
@@ -1,41 +1,27 @@
-import React from 'react'
+import { useContext } from 'react'
+import CarouselHowToPlay from '../../components/carousel/CarouselHowToPlay'
+import { LanguageContext } from '../../context/LanguageContext'
 
 const HowToPlayScreen = () => {
+  const { text } = useContext(LanguageContext)
   return (
-    <main className='lg:h-[78.8%] lg:mt-10 animate__animated animate__fadeIn'>
-      <span className='text-sky-700 font-semibold'>Getting started</span>
-      <h1 className='font-semibold text-slate-900'>
-        How To Play Billionaire eCommerce!
-      </h1>
-
-      <span className='text-gray-500 font-medium'>
-        This application allow you to simulate the wealth of the top 10 forbes
-        human beeings of the planet. and get a grasp of the adquisition power,
-        by consuming an API and with some frontend magic i came up with this
-        idea to combine the data from forbes400 api and my interest for
-        E-Commerce apps to show how much it means to be within the top %1 of the
-        population.
-      </span>
-
-      <div className='flex items-start gap-2 mt-10'>
-        <span className='text-white font-medium bg-slate-600 px-2 py-1 rounded text-sm'>
-          1
-        </span>
-        <div className='flex flex-col'>
-          <span className='font-semibold text-slate-800'>Login</span>
-          <span>First step is to login in order to be able to buy stuff!!</span>
-          <ul className='list-disc pl-5'>
-            <li>Create a new user with username/password</li>
-            <li>Sign in with google button</li>
-            <li>
-              Use guest account{' '}
-              <span className='inline-block py-[0.15rem] px-1 leading-none text-center whitespace-nowrap align-baseline font-medium bg-slate-400 text-white rounded'>
-                recommended
-              </span>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <main className='lg:h-[78.8%] shadow-2xl bg-gradient-to-br from-slate-200/95 via-gray-200/40 to-blue-200/50 rounded-md flex animate__animated animate__fadeIn'>
+      <section className='hidden lg:block w-2/5 h-full p-0 m-0 group'>
+        <article className='flex justify-center items-center h-4/5'>
+          <div className='flex flex-col items-center gap-y-7'>
+            <h1 className='font-medium text-5xl text-gray-600'>
+              {text.howToPlay}
+            </h1>
+            <i className='fa-solid fa-person-chalkboard text-7xl text-gray-600 group-hover:scale-110 group-hover:-rotate-3 duration-300'></i>
+            <span className='text-gray-600 text-lg'>
+              {text.howToPlayInstructions}
+            </span>
+          </div>
+        </article>
+      </section>
+      <section className='bg w-full lg:w-2/3 h-full p-0 m-0'>
+        <CarouselHowToPlay />
+      </section>
     </main>
   )
 }
