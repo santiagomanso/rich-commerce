bb34de90fa629cab2ff9b5c32128e40a02aa0735
santiagomanso <santiagomanso@web.de>
2023-01-24 13:04:03 +0100

    ⚰️ removed useEffect

--- src/hooks/useCategories.js
+++ src/hooks/useCategories.js
@@ -1,18 +1,16 @@
 // 23.11.2022  this will be implemented after the design is operational.
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { categories_list } from '../data/categories'
 
 const useCategories = () => {
   const [categories, setCategories] = useState(categories_list)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
 
-  useEffect(() => {
-    setCategories(categories_list)
-  }, [])
+  //for future development when there is an actual rest api
+  // const [loading, setLoading] = useState(false)
+  // const [error, setError] = useState('')
 
-  return { loading, error, categories, setCategories }
+  return { categories, setCategories }
 }
 
 export default useCategories
