cb54e517f9da7d02d7ca10d21294d7c0fb2ddcfa
santiagomanso <santiagomanso@web.de>
2023-01-27 14:52:13 +0100

    🚑 fixed problem with enter on guest/submit reload

--- src/Pages/LoginScreen/LoginScreen.jsx
+++ src/Pages/LoginScreen/LoginScreen.jsx
@@ -103,10 +103,7 @@ const LoginScreen = () => {
             {text.screenLogin}
           </h2>
 
-          <form
-            className='flex flex-col gap-2 mt-4 relative'
-            onSubmit={(e) => handleSubmit(e)}
-          >
+          <form className='flex flex-col gap-2 mt-4 relative'>
             <label className='flex flex-col'>
               <span>{text.email}</span>
               <input
@@ -144,6 +141,7 @@ const LoginScreen = () => {
                   name='password'
                   id=''
                   placeholder={text.password}
+                  onClick={() => setError(null)}
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
@@ -151,7 +149,8 @@ const LoginScreen = () => {
 
             <div className='flex items-stretch justify-between mt-4'>
               <button
-                onClick={() => handleGuest}
+                type='button'
+                onClick={(e) => handleGuest(e)}
                 className='w-[45%] shadow-md bg-gradient-to-br from-indigo-500/90 to-purple-500/80 text-gray-200 rounded-lg py-2 flex justify-center items-center gap-1 border font-semibold hover:scale-105 duration-300'
               >
                 <svg
@@ -165,15 +164,13 @@ const LoginScreen = () => {
                   <path d='M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z' />
                   <path d='M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0h-7zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492V2.5z' />
                 </svg>
-                <span className='text-gray-100 lg:hidden'>
-                  {text.buttonUseGuestAccount}
-                </span>
                 <span className='hidden lg:block'>
                   {text.buttonUseGuestAccount}
                 </span>
               </button>
               <button
                 type='submit'
+                onClick={(e) => handleSubmit(e)}
                 className='w-[45%] shadow-md bg-gradient-to-br from-gray-400/20 to-slate-700/70 text-gray-200 tracking-wider py-2 rounded-md font-bold hover:scale-105 duration-300 border'
               >
                 {text.buttonLogin}
