21a92bfa249160fe207fe82e89b968560e096724
santiagomanso <santiagomanso@web.de>
2023-02-02 13:17:40 +0100

    ♻️ reworked UserContext, no more hook like.

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
-import { UserAuth } from '../../context/AuthContext'
+import { useContext } from 'react'
+import { UserContext } from '../../context/AuthContext'
 import RedirectMsg from '../../Pages/RedirectMsg/RedirectMsg'
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = UserAuth()
+  const { user } = useContext(UserContext)
 
   if (!user) {
     return <RedirectMsg />
