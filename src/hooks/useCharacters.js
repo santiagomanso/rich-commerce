64432f7182e764419286fc9721083d1a38fa99a4
santiagomanso <santiagomanso@web.de>
2023-04-20 22:26:05 +0200

    📝 renamed promises => better pull request comment

--- src/hooks/useCharacters.js
+++ src/hooks/useCharacters.js
@@ -10,14 +10,14 @@ const useCharacters = () => {
   const fetchData = async () => {
     setLoading(true)
     try {
-      const responsePromise = axios.get(
+      const responseOriginal = axios.get(
         'https://forbes400.onrender.com/api/forbes400?limit=15',
       )
-      const timeoutPromise = new Promise((_, reject) =>
+      const responseTimedOut = new Promise((_, reject) =>
         setTimeout(() => reject(new Error('Request timed out')), 2000),
       )
       //promise.race takes an array of promises but returns the one that resolves the fastest
-      const { data } = await Promise.race([responsePromise, timeoutPromise])
+      const { data } = await Promise.race([responseOriginal, responseTimedOut])
       setCharacters(data)
       setLoading(false)
     } catch (error) {
